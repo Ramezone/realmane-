896de5696c4ce785ba1018834bd3edc9dc2b7225
Rambo Allie <135633204+Ramezone@users.noreply.github.com>
2023-12-02 15:48:11 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,30 @@
 const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
 const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello, World!\n');
+  if (req.url === '/' || req.url === '/index.html') {
+    // Read the index.html file
+    const indexPath = path.join(__dirname, 'index.html');
+
+    fs.readFile(indexPath, 'utf8', (err, data) => {
+      if (err) {
+        res.writeHead(500, { 'Content-Type': 'text/plain' });
+        res.end('Internal Server Error');
+        console.error(err);
+        return;
+      }
+
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.end(data);
+    });
+  } else {
+    // Handle other routes or resources here
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('Not Found');
+  }
 });
 
 server.listen(port, () => {
